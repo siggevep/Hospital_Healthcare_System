09513a4402e31bbfad85d70c9070906d3f82f5db
FatimaAlmurtadha <sukaina77150@gmail.com>
2025-10-10 13:15:42 +0200

    Creating a constructor to Journal class

--- Journal.cs
+++ Journal.cs
@@ -7,4 +7,14 @@ class Journal
   string? Titel;
   string? Description;
   string? Attechment;
+
+  public Journal (string? patientname, string? date, string? titel, string? description, string? attechment)
+  {
+    PatientName = patientname;
+    Date = date;
+    Titel = titel;
+    Description = description;
+    Attechment = attechment;
+    
+  }
 }
