2971e1cd4ef9a02b1c01005343c37035eec82f8d
FatimaAlmurtadha <sukaina77150@gmail.com>
2025-10-10 13:23:23 +0200

    Creat Geters methods for Journal's information

--- Journal.cs
+++ Journal.cs
@@ -6,15 +6,36 @@ class Journal
   string? Date;
   string? Titel;
   string? Description;
-  string? Attechment;
+  string? Attachment;
 
-  public Journal (string? patientname, string? date, string? titel, string? description, string? attechment)
+  public Journal(string? patientname, string? date, string? titel, string? description, string? attachment)
   {
     PatientName = patientname;
     Date = date;
     Titel = titel;
     Description = description;
-    Attechment = attechment;
-    
+    Attachment = attachment;
+
+  }
+
+  public string? GetPatientName()
+  {
+    return PatientName;
+  }
+  public string? GetDate()
+  {
+    return Date;
+  }
+  public string? GetTitel()
+  {
+    return Titel;
+  }
+  public string? GetDescription()
+  {
+    return Description;
+  }
+  public string? GetAttatchment()
+  {
+    return Attachment;
   }
 }
