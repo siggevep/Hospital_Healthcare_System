b87ad57f55025614825ee2a07b1bd5b5824f3808
Sigge von Eggers Patron <sigge.voneggerspatr@learnet.se>
2025-10-13 14:09:25 +0200

    Added so if you want to give someone local admin, Thier name and password

--- Program.cs
+++ Program.cs
@@ -18,22 +18,25 @@ Admin_meny();
 static void Give_Admin_permision()
 {
     Console.WriteLine("Give a User the Admin permissions in that location// type the name och the person to give Admin permissions");
-     foreach (user in users)
-    {
-
-        Console.WriteLine(users.username);
+    string input = Console.ReadLine();
+    string input_2 = Console.ReadLine();
 
 
 
-    }
+    foreach (IUser user in Users)
+        if (user.TryLogin(input, input_2))
+        {
+            user.GetRole(Local_Admin);
+            File.WriteAllText("./Local_Admin.txt", user.username + "  " + user.password);
+            break;
+
+        }
+
+
+
     Console.WriteLine("Write one of theese names to give them admin");
 
-    string input = Console.ReadLine()!;
-    
-    
-  
-
-
+    string input = Console.ReadLine()!; 
 
 
 }
