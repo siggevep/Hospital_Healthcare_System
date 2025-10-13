bfd22b84688f6ea77205c91b5bd0f244b6e94452
FatimaAlmurtadha <sukaina77150@gmail.com>
2025-10-13 11:39:12 +0200

    Solve pushing up

---
+++ Local_Admin_Permission.cs
@@ -0,0 +1,14 @@
+
+namespace App;
+
+class Local_Admin_Permission
+{
+  // Add new locations
+  public static void NewLocation(string? LocationName, string? HospitalName)
+  {
+    List<string?> locations = new List<string?>();
+    locations.Add(LocationName, HospitalName);
+  }
+
+
+}
