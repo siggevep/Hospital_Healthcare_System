52e7ea7c1a6b3fda0067bbb5878ef2b4d7110fd6
Sigge von Eggers Patron <sigge.voneggerspatr@learnet.se>
2025-10-13 13:16:18 +0200

    Adding Code so Main_Admin can give other Users Admin

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using App;
+﻿using System.Reflection.Metadata;
+using App;
 static void Admin_meny()
 {
     Console.WriteLine("1. To give admins more permissions in their respective Location");
@@ -12,3 +13,27 @@ static void Admin_meny()
 
 
 Admin_meny();
+
+
+static void Give_Admin_permision()
+{
+    Console.WriteLine("Give a User the Admin permissions in that location// type the name och the person to give Admin permissions");
+     foreach (user in users)
+    {
+
+        Console.WriteLine(users.username);
+
+
+
+    }
+    Console.WriteLine("Write one of theese names to give them admin");
+
+    string input = Console.ReadLine()!;
+    
+    
+  
+
+
+
+
+}
