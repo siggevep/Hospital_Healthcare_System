14dc6a01947a3afe8b991cf2cc9c9ac785d227f6
FatimaAlmurtadha <sukaina77150@gmail.com>
2025-10-13 11:30:14 +0200

    conflict

--- obj/Debug/net8.0/Hostipal.AssemblyInfo.cs
+++ obj/Debug/net8.0/Hostipal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hostipal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2bb55cab5914340633db3e9285fbd51b004bf61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c30c74ce994b28af25a17e3673111411aa0a4de9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hostipal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hostipal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
