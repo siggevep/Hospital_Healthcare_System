61f962b81c9b5ad983079ba59357b1683e6b90f4
Sigge von Eggers Patron <sigge.voneggerspatr@learnet.se>
2025-10-13 11:28:18 +0200

    Commit for Admin_meny

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using App;
+static void Admin_meny()
+{
+    Console.WriteLine("1. To give admins more permissions in their respective Location");
+    Console.WriteLine("2. To give an admin a specific region");
+    Console.WriteLine("3. To Handle registrations");
+    Console.WriteLine("4. To Add locations");
+    Console.WriteLine("5. To accept/deny user registration to become patients");
+    Console.WriteLine("6. To create Personnel accounts");
+    Console.WriteLine("7. Watch the acces list ");
+}
+
+
+Admin_meny();
