984e47c367a017879cc83a63d6b2e43b36bf8d54
FatimaAlmurtadha <sukaina77150@gmail.com>
2025-10-15 09:44:45 +0200

    fixa conflict

--- obj/Debug/net8.0/Hostipal.AssemblyInfo.cs
+++ obj/Debug/net8.0/Hostipal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hostipal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e311d737d5e9e3afa069cd505d6a94c9f74aa35a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+285baa767954ce75ec7697bb854d556f622e7390")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hostipal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hostipal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
