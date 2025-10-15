ed9c05c6698a8c355896e77dfdfeb99833b92e22
alireza8850 <alireza.it101@gmail.com>
2025-10-15 13:06:45 +0200

    ---

--- obj/Debug/net8.0/Hostipal.AssemblyInfo.cs
+++ obj/Debug/net8.0/Hostipal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hostipal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40258f5f566941f9fc38dbef9bbaf6aae5eab72a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db68e8e61961691a5201ac74095d459f5e0ca7d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hostipal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hostipal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
